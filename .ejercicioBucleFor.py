66ed3d322a6d61f60f0caacf531eaca4bfe0c7eb
Alex <alexalonsoportas@gmail.com>
2021-01-05 21:38:19 +0100

    correcion ejercicio en el rango

--- .ejercicioBucleFor.py
+++ .ejercicioBucleFor.py
@@ -10,7 +10,7 @@ def comparaListas(lista1, lista2):
     if len(lista1)!=len(lista2):
         return False
     else:
-        for i in range(1,len(lista1)):
+        for i in range(0,len(lista1)):
             if lista1[i]!=lista2[i]:
                 return False
     return True
