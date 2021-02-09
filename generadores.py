9089533d4061e614c4bbeac8d853dcbfec277df2
Alex <alexalonsoportas@gmail.com>
2021-02-09 15:08:24 +0100

    generadores

---
+++ generadores.py
@@ -0,0 +1,21 @@
+def generaPares(limite):        #funcion
+    num = 1
+    pares = []
+    while num<limite:
+        pares.append(num*2)
+        num+=1 
+    return pares
+
+print(generaPares(6))
+
+def generadorPares(limite):        #generador
+    num = 1
+    while num<limite:
+        yield num*2
+        num+=1 
+    return pares
+
+pares = generadorPares(6)
+
+for i in pares:
+    print(i)
