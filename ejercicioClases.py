3e12c4fb019fdcdff3fa016de73f95078b2f0b7c
Alex <alexalonsoportas@gmail.com>
2021-02-22 10:13:02 +0100

    ejercicio clases e herencias

--- ejercicioClases.py
+++ ejercicioClases.py
@@ -1,4 +1,4 @@
-class Cuenta():
+class CuentaCorriente():
     n_cuenta = ""
     titular = ""
     saldo = 0
@@ -16,11 +16,34 @@ class Cuenta():
         
     def getDatos(self):
         return "Nº cuenta: " + self.n_cuenta + ", titular: " + self.titular + ", saldo: " + str(self.saldo)
+
+class CuentaJoven(CuentaCorriente):
+    def __init__(self, n_cuenta, titular, saldo, bonus_promocion=0):
+        super().__init__(n_cuenta, titular, saldo)
+        self.bonus = bonus_promocion
+        self.saldo += bonus_promocion
     
-p1 = Cuenta(1234, "masculino", 100)
+    def getBonus(self):
+        return "El bonus promocional es de " + str(self.bonus)
 
-p1.ingresar(50)
+    def getDatos(self):
+        return super().getDatos() + ", bonus: " + str(self.bonus)
 
-p1.retirar(75)
 
-print(p1.getDatos())
+c1 = CuentaCorriente(1234, "masculino", 100)
+
+c1.ingresar(50)
+
+c1.retirar(75)
+
+print(c1.getDatos())
+
+c2 = CuentaJoven("ES12425O134", "Pepe Masculino", 100, 30)
+
+print(c2.getBonus())
+
+c2.ingresar(50)
+
+c2.retirar(75)
+
+print(c2.getDatos())
