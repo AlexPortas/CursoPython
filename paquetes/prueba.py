cdfeeaa13a05f1afb6d1b343659891b848e66e51
Alex <alexalonsoportas@gmail.com>
2021-03-17 16:58:52 +0100

    paquetes distribuidos

---
+++ paquetes/prueba.py
@@ -0,0 +1,27 @@
+import modulos.modulos
+
+modulos.modulos.sumar(7,5)
+
+modulos.modulos.restar(7,5)
+
+modulos.modulos.multiplicar(7,5)
+
+print("---------------------------")
+
+from modulos.modulos import sumar, restar, multiplicar
+
+sumar(17,5)
+
+restar(17,5)
+
+multiplicar(17,5)
+
+print("---------------------------")
+
+from modulos.modulos import *
+
+sumar(17,52)
+
+restar(17,25)
+
+multiplicar(17,52)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "funciones",
+    version = "1.0",
+    author = "Alex",
+    author_mail = "",
+    packages = ["modulos"]
+)
