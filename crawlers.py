f44367e8ee1320147fee03b741b89e05ca4f200e
Alex <alexalonsoportas@gmail.com>
2021-11-26 11:18:08 +0100

    crawlers poo

--- crawlers.py
+++ crawlers.py
@@ -2,6 +2,8 @@ import requests
 
 from bs4 import BeautifulSoup
 
+from urllib.parse import urljoin
+
 class PostCrawled():
 
     def __init__(self, titulo, emoyi, contenido, imagen):
@@ -13,14 +15,15 @@ class PostCrawled():
 class PostExtractor():
 
     def extraerInfo(self):
-        r = requests.get('http://python.beispiel.programmierenlernen.io/index.php')
+        urlBase = 'http://python.beispiel.programmierenlernen.io/index.php'
+        r = requests.get(urlBase)
         documento = BeautifulSoup(r.text, "html.parser")
         posts = []
         for card in documento.select(".card-block"):
             titulo = card.select(".card-title span")[1].text
             emoticono = card.select_one(".emoji").text
             contenido = card.select_one(".card-text").text
-            imagen = card.select_one("img").attrs["src"]
+            imagen = urljoin(urlBase,card.select_one("img").attrs["src"])
 
             crawled = PostCrawled(titulo,emoticono, contenido, imagen)
             posts.append(crawled)
