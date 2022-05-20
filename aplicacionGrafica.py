edd0ef35e25b1e90db5092f2dc87968ccf1dbd35
Alex <alexalonsoportas@gmail.com>
2022-05-20 20:51:02 +0200

    tkinter

---
+++ aplicacionGrafica.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+raiz=Tk()
+
+raiz.title("Primera ventana tkinter")
+
+raiz.resizable(0,0)
+
+# cambiar imagen!! tiene que ser un .ico!!  raiz.iconbitmap("favicon.ico")
+
+raiz.geometry("500x450")
+
+raiz.config(bg="blue")
+
+raiz.mainloop()
