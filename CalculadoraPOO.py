b3475c3b067343b51dd30915fdda7ca8e858ff82
Alex <alexalonsoportas@gmail.com>
2022-07-28 13:12:25 +0200

    usar expresion regular en *

--- CalculadoraPOO.py
+++ CalculadoraPOO.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import re
 
 raiz=Tk()
 
@@ -6,6 +7,7 @@ class Calculadora:
     def __init__(self, ventana):
         self.ventana=ventana
         self.ventana.title('Calculadora')
+        self.operacion = ""
 
         #agregar display
         self.display=Entry(ventana, font=('Arial 22'))
@@ -18,22 +20,22 @@ class Calculadora:
         btn1=self.colocarBtn(1)
         btn2=self.colocarBtn(2)
         btn3=self.colocarBtn(3)
-        btnDiv=self.colocarBtn('/', mostrar=False)
+        btnDiv=self.colocarBtn('/')
 
         btn4=self.colocarBtn(4)
         btn5=self.colocarBtn(5)
         btn6=self.colocarBtn(6)
-        btnMult=self.colocarBtn('*', mostrar=False)
+        btnMult=self.colocarBtn(u"\u00d7")
 
         btn7=self.colocarBtn(7)
         btn8=self.colocarBtn(8)
         btn9=self.colocarBtn(9)
-        btnRest=self.colocarBtn('-', mostrar=False)
+        btnRest=self.colocarBtn('-')
 
         btnResul=self.colocarBtn('=', mostrar=False)
         btn0=self.colocarBtn(0)
         btnComa=self.colocarBtn('.')
-        btnSum=self.colocarBtn('+', mostrar=False)
+        btnSum=self.colocarBtn('+')
 
         botones=[btn1, btn2, btn3, btnDiv, btn4, btn5, btn6, btnMult, btn7, btn8, btn9, btnRest, btnResul, btn0, btnComa, btnSum]
         contador=0
@@ -43,8 +45,23 @@ class Calculadora:
                 contador+=1 
 
     def colocarBtn(self, valor, mostrar=True):
-        return Button(self.ventana, text=valor, width=10, font=('Courier', 9))
+        return Button(self.ventana, text=valor, width=10, font=('Courier', 9), command=lambda:self.pulsacionesTeclas(valor, mostrar))
 
-calcu= Calculadora(raiz)
+    def pulsacionesTeclas(self, valor, mostrar):
+        if mostrar:
+            self.operacion+=str(valor)
+            self.mostrarDisplay(valor)
+        else:
+            self.operacion=re.sub(u"\u00d7", "*", self.operacion)
+            self.borrarDisplay()
+            self.mostrarDisplay(str(eval(self.operacion)))
+
+    def mostrarDisplay(self, valor):
+        self.display.insert(END, valor)
+
+    def borrarDisplay(self):
+        self.display.delete(0, END)
+
+calcu = Calculadora(raiz)
 
 raiz.mainloop()
