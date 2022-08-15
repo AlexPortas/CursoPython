4f939dfa9f7d6a9e7c6bfb1c8ae528f5bd797fc1
Alex <alexalonsoportas@gmail.com>
2022-08-15 15:09:49 +0200

    insertar y mostrar datos de una base de datos

--- trabajo con bbdd/PracticaDatos.py
+++ trabajo con bbdd/PracticaDatos.py
@@ -5,9 +5,21 @@ conexion=sqlite3.connect("trabajo con bbdd/db")
 cursor=conexion.cursor()
 
 #cursor.execute("CREATE TABLE PRODUCTOS (ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
-cursor.execute(" INSERT INTO PRODUCTOS VALUES('Camiseta',15,'Ropa')")
+#cursor.execute(" INSERT INTO PRODUCTOS VALUES('Camiseta',15,'Ropa')")
+'''productos=[
+("Tenis", 100, "Deportes"),
+("Pantalón", 58, "Ropa"),
+("Reloj", 40, "Joyas")
+]
 
-conexion.commit()
+cursor.executemany("INSERT INTO PRODUCTOS VALUES(?, ?, ?)", productos)'''
+
+cursor.execute("SELECT * FROM PRODUCTOS")
+
+productos=cursor.fetchall()
+
+print(productos)
+#conexion.commit()
 
 cursor.close()
 
