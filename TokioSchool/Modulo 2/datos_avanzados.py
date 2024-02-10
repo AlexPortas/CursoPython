16a48cc3ec5da67b35055c61af1b20be5cc8a469
Alex <alexalonsoportas@gmail.com>
2024-02-10 22:22:15 +0100

    salida y listas

---
+++ TokioSchool/Modulo 2/datos_avanzados.py
@@ -0,0 +1,18 @@
+# lista
+
+datos = [4,"Una cadena",-15,3.14,"Otra cadena"]
+print(datos)
+
+# agregar a lista
+
+datos.append(True)
+datos.insert(0, 7)
+print(datos)
+
+# buscar en lista
+
+print(4 in datos)
+
+#contar valores lista
+
+print(len(datos))
