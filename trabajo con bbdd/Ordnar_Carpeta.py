4e24c6167f0adbb73bd72fef30eb1785a436b394
Alex <alexalonsoportas@gmail.com>
2025-04-30 19:36:23 +0200

    con dict

--- trabajo con bbdd/Ordnar_Carpeta.py
+++ trabajo con bbdd/Ordnar_Carpeta.py
@@ -5,17 +5,23 @@ import shutil
 ruta="C:/Users/Usuario/Desktop/carpeta"
 
 #crear carpetas si no existen
-tipos=["IMG","PDFs", "doc_texto"]
-
-for carpeta in tipos:
+#tipos=["IMG","PDFs", "doc_texto"]
+extensiones={
+    ".jpg":"IMG",
+    ".png":"IMG",
+    ".pdf":"PDFs",
+    ".txt":"doc_texto",
+    ".odt":"doc_texto",
+}
+for carpeta in set(extensiones.values()):
     nuevaCarpeta=os.path.join(ruta,carpeta)
     if not os.path.exists(nuevaCarpeta):
         os.mkdir(nuevaCarpeta)
 
 for archivo in os.listdir(ruta):
-    if archivo.endswith(".jpg") or archivo.endswith(".png"):
-        shutil.move(os.path.join(ruta,archivo),os.path.join(ruta,"IMG"))
-    if archivo.endswith(".pdf"):
-        shutil.move(os.path.join(ruta,archivo),os.path.join(ruta,"PDFsG"))
-    if archivo.endswith(".doc") or archivo.endswith(".odt"):
-        shutil.move(os.path.join(ruta,archivo),os.path.join(ruta,"doc_texto"))
+    nuevoArchivo=os.path.join(ruta,archivo)
+    if os.path.isfile(nuevoArchivo):
+        nane,ext=os.path.splitext(archivo)
+        if ext in extensiones:
+            destino=os.path.join(ruta,extensiones[ext],archivo)
+            shutil.move(nuevoArchivo,destino)
