955f0e9aba9c9dbd54f52b945fcf268095ccf6af
Alex <alexalonsoportas@gmail.com>
2025-05-25 21:01:02 +0200

    renombrar

--- Renombrar_Archivos.py
+++ Renombrar_Archivos.py
@@ -2,9 +2,16 @@ import os
 
 ruta="C:/Users/Usuario/Desktop/carpeta/IMG"
 prefijo="imagen_"
-extension=".jpg"
+extension=(".png",".jpg")
 
 archivos=[]
 for f in os.listdir(ruta):
     if f.endswith(extension):
         archivos.append(f)
+
+for i, archivo in enumerate(archivos, start=1):
+    ext_actual=os.path.splitext(archivo)[1]
+    nombre=f"{prefijo}{i:03}{ext_actual}"
+    ruta_actual=os.path.join(ruta,archivo)
+    ruta_nueva  =os.path.join(ruta,nombre)
+    os.rename(ruta_actual,ruta_nueva)
