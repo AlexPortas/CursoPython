0851e93f6ad6ec81038824f3efc0a56254ce1a0d
Alex <alexalonsoportas@gmail.com>
2025-10-03 18:34:09 +0200

    poner servidor en desarrollo

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, url_for
+import os
 
 app=Flask(__name__)
+
+empleados=["Alex", "Ana", "Bea", "Eva", "Pepe"
+".0"]
 @app.route('/')
 def saludo():
-    return render_template("index.html")
+    return render_template("index.html", empleados=len(empleados))
+
+if __name__=="__main__":
+    os.environ['FLASK_ENV']="development"
+    app.run(debug=True)
