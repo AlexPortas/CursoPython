b35ffbd97d0788e17a2013da0115b45c7ef028df
Alex <alexalonsoportas@gmail.com>
2025-10-03 20:50:45 +0200

    RUTAS FLASK

--- Flask/app.py
+++ Flask/app.py
@@ -3,11 +3,18 @@ import os
 
 app=Flask(__name__)
 
-empleados=["Alex", "Ana", "Bea", "Eva", "Pepe"
-".0"]
+empleados=["Alex", "Ana", "Bea", "Eva", "Pepe"]
 @app.route('/')
 def saludo():
     return render_template("index.html", empleados=len(empleados))
+
+@app.route('/quienes')
+def quienes():
+    return render_template("aboutus.html", empleados=empleados)
+
+@app.route('/usuarios/<string:usu>')
+def usuarios(usu):
+    return "El usuario es"+usu
 
 if __name__=="__main__":
     os.environ['FLASK_ENV']="development"
