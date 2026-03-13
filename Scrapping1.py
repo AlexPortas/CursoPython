bfdcd3fc0db371c183964ee072e8666c40d8cfd8
Alex <alexalonsoportas@gmail.com>
2026-03-13 13:06:34 +0100

    scrapping

--- Scrapping1.py
+++ Scrapping1.py
@@ -1,26 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
-
+import time
 
 base="https://quotes.toscrape.com/page/"
 
-with open("citas3.txf", "w", encoding="utf-8") as fichero:
+with open("citas_autor.txf", "w", encoding="utf-8") as fichero:
     for pagina in range(1, 4):
 
         url= base+str(pagina)+"/"
 
         resp=requests.get(url)
 
-        #print(resp.status_code)
+        if resp.status_code==200:
 
-        html=resp.text
+            html=resp.text
 
-        #print(html[:500])
+            #print(html[:500])
 
-        soup=BeautifulSoup(html, 'html.parser')
-        citas=soup.find_all('div', class_='quote')
-        for cita in citas:
-            texto=cita.find('span', class_='text').get_text()
-            autor=cita.find('small', class_='author').get_text()
-
-            fichero.write(f"{texto} -> {autor}\n")
+            soup=BeautifulSoup(html, 'html.parser')
+            citas=soup.find_all('div', class_='quote')
+            for cita in citas:
+                texto=cita.find('span', class_='text').get_text()
+                autor=cita.find('small', class_='author').get_text()
+                #obtener ruta del autor
+                enlace=cita.find("a")["href"]
+                url_autor="https://quotes.toscrape.com/"+enlace
+                resp_aut=requests.get(url_autor)
+                if resp_aut.status_code==200:
+                    soup=BeautifulSoup(resp_aut.text, 'html.parser')
+                    data_nac=soup.find('span', class_='author-born-date').get_text()
+                    lugar_nac=soup.find('span', class_='author-born-location').get_text()
+                    fichero.write(f"{texto} -> {autor} - {data_nac} - {lugar_nac} \n")
+        else:
+            print(f"Error al leer la pagina {pagina}")
+        time.sleep(1)
