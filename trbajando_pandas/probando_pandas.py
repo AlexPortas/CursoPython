e5419277ad5193fd3d8685554b54bfb6d7d3aeac
Alex <alexalonsoportas@gmail.com>
2026-04-04 20:46:04 +0200

    pandas

---
+++ trbajando_pandas/probando_pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# aguacates=pd.read_excel("C:\\Users\\Usuario\\Downloads\\video141\\Aguacate producción.xlsx")
+# print(aguacates)
+
+aguacates=pd.read_csv("C:\\Users\\Usuario\\Downloads\\video141\\aguacate.csv")
+print(aguacates)
